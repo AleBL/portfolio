2375f02d23d2b637494ac3db6133de1420df4c1b
Alessandro B <a.barros@manusis4.com>
2024-11-20 18:03:19 -0300

    particle classes removed, using particles.js file

--- src/ts/Canvas/Canvas.ts
+++ src/ts/Canvas/Canvas.ts
@@ -1,29 +1,8 @@
 // Connect the canvas element to a Particles class
 
-import Particles from '../Particles/Particles'
-import { Stars } from './Stars'
-import { Sections, ScrollHook } from '../Modules/WebPage'
-
-let canvas = new Particles('#particles', '2d');
-canvas.setParticleSettings(Stars.Particles);
-canvas.setInteractiveSettings(Stars.Interactive);
-canvas.start();
-
-let paused = false;
+import { ScrollHook } from '../Modules/WebPage'
 
 ScrollHook.addEventListener('scroll', () => {
-    if(Sections.get('canvas').inView()) {
-        if(paused) {
-            paused = false;
-            canvas.resume();
-        }
-    }
-    else {
-        if(!paused) {
-            paused = true;
-            canvas.pause();
-        }
-    }
 },
 {
     capture: true,
--- src/ts/Particles/Mouse.ts
+++
@@ -1,6 +0,0 @@
-import Coordinate from './Coordinate'
-
-export default interface Mouse {
-    position: Coordinate,
-    over: boolean
-}
