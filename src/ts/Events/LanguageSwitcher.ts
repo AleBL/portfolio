b260f1aff716812615e013afc900a10cee6ccf32
Alessandro B <ale_alessandro_ale@hotmail.com>
2024-11-21 19:26:15 -0300

    created button for language switching

---
+++ src/ts/Events/LanguageSwitcher.ts
@@ -0,0 +1,3 @@
+import LanguageSwitcher from '../Classes/Component/LanguageSwitcher';
+
+document.addEventListener('DOMContentLoaded', () => { const languageSwitcher = new LanguageSwitcher('language-switcher'); });
