d1244706a8f2b995d05bcb69b685895926dab530
Alessandro Barros <ale_alessandro_ale@hotmail.com>
2024-11-25 15:27:49 -0300

    refact/change particle effect (#1)
    * particle classes removed, using particles.js file
    
    * updated dependency caniuse-lite

--- src/ts/Particles/Animation.ts
+++
@@ -1,5 +0,0 @@
-// Class to hold information regarding how a particle should animate
-
-export default class Animation {
-    constructor(public speed: number, public max: number, public min: number, public increasing: boolean = false) { }
-}
