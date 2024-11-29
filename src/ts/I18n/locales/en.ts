63f5f2ecf3b1dc95066c70b16c4d399a0b2cd3a3
Alessandro B <ale_alessandro_ale@hotmail.com>
2024-11-29 16:53:19 -0300

    added English translations for experiences.

--- src/ts/I18n/locales/en.ts
+++ src/ts/I18n/locales/en.ts
@@ -60,6 +60,85 @@ const en = {
         experience: {
             title: "Experience",
             short_text: "Your professional journey",
+            work_mode: {
+                remote: "Remote",
+                in_person: "In-Person",
+            },
+            manusis: {
+                text: "Manusis4 is a company specialized in maintenance and asset management solutions, providing tools for process optimization, preventive maintenance, inventory control, and integration with ERP and IoT systems, aiming to increase efficiency and reduce operational costs.",
+                rds_squad: {
+                    name: "RDS (Research and Development Squad)",
+                    position: "Mid-Level Full Stack Developer",
+                    roles: {
+                        0: "Objective to migrate the application to more recent technologies.",
+                        1: "Implementation of more efficient solutions.",
+                        2: "Development of optimized endpoints.",
+                        3: "Creation of reusable components in the front-end.",
+                        4: "Continuous improvements in the architecture, aiming to move away from the rigid monolithic architecture.",
+                    },
+                },
+                prs_squad: {
+                    name: "PRS (Projects Squad)",
+                    position: "Mid-Level Full Stack Developer",
+                    roles: {
+                        0: "Focused on new developments according to client demands.",
+                        1: "Aiming to deliver effective and quality solutions.",
+                    },
+                },
+                sts_squad: {
+                    name: "STS (Sustaining Squad)",
+                    position: "Mid-Level Full Stack Developer",
+                    roles: {
+                        0: "Main responsibility was to solve bugs and adjust business rules quickly.",
+                        1: "Ensuring application continuity and stability.",
+                    },
+                },
+            },
+            stefanini: {
+                text: "Stefanini is a global company with over 35 years of experience, specializing in digital transformation solutions, software development, IT infrastructure, automation, and consulting. We serve various industries, including automotive, healthcare, retail, and manufacturing, helping our clients innovate and improve their operational efficiency.",
+                vale_team: {
+                    name: "Vale Team",
+                    position: "Software Support Engineer",
+                    roles: {
+                        0: "Constant monitoring of system performance.",
+                        1: "Identification of areas for improvement.",
+                        2: "Proposing proactive solutions to prevent interruptions.",
+                        3: "Performing N2 and N3 technical support functions.",
+                        4: "Providing specialized assistance to solve complex software-related issues.",
+                        5: "Adhering to established SLAs.",
+                        6: "Ensuring service targets were met effectively.",
+                    },
+                },
+            },
+            james: {
+                text: "James was a delivery platform that connects users to a variety of products, including food, supermarkets, and other items, with quick and efficient deliveries carried out by an established network of sellers. Our goal was to save customers time by offering a practical and convenient shopping experience.",
+                payment_squad: {
+                    name: "Payments Team",
+                    position: "Mid-Level Backend Software Developer",
+                    roles: {
+                        0: "Development of the JamesPay payment gateway.",
+                        1: "Microservices architecture to reduce costs and expand payment options.",
+                        2: "Inclusion of debit cards and Pix, in addition to credit transactions.",
+                        3: "Continuous integration and adjustments during the transition from the monolithic architecture.",
+                    },
+                },
+            },
+            teresina_city_hall: {
+                name: "Teresina City Hall",
+                text: "The Municipal Finance Secretariat of Teresina (SEMF) is responsible for the city’s tax and financial administration. It handles general accounting, expense settlement and payment, and seeks to ensure a fair and efficient tax policy. SEMF also offers various online services, such as consultations and payments of IPTU, ITBI, and other municipal fees.",
+                finance_department_team: {
+                    name: "Finance Department Team",
+                    position: "Junior Full Stack Developer",
+                    roles: {
+                        0: "Management of the Pruning System: I was responsible for the urban pruning system, ensuring the workflow was functional and organized.",
+                        1: "Tree Analysis Automation: Developed software to automate the analysis of cut trees, replacing manual processes with an advanced digital solution.",
+                        2: "Increased Operational Efficiency: Automation of the system reduced time spent on analyses, optimizing workflow and freeing up resources for other activities.",
+                        3: "Improved Decision Making: The software provided more precise and accessible data, enabling quicker and more assertive decisions by the management team.",
+                        4: "Bug Fixing: Identified and resolved technical issues in the system, ensuring stability and reliability in daily use.",
+                        5: "Updating Business Rules: Adapted the system to new needs and demands, implementing changes in business rules as requested by the team.",
+                    },
+                },
+            }
         },
         projects: {
             title: "Projects",
